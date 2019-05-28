20a58ec45e530e0cd89e75a559ecc0be9c6432b6
Armen <armen_bagramian@gmail.com>
2019-05-28 17:19:23 +0200

    chat done

--- db/schema.rb
+++ db/schema.rb
@@ -1,9 +1,25 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_28_110732) do
-
+ActiveRecord::Schema.define(version: 2019_05_28_141422) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "chat_rooms", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "meetings", force: :cascade do |t|
     t.bigint "users_id"
@@ -11,6 +27,16 @@ ActiveRecord::Schema.define(version: 2019_05_28_110732) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["users_id"], name: "index_meetings_on_users_id"
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "chat_room_id"
+    t.string "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["chat_room_id"], name: "index_messages_on_chat_room_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
   create_table "reviews", force: :cascade do |t|
@@ -43,17 +69,16 @@ ActiveRecord::Schema.define(version: 2019_05_28_110732) do
     t.text "bio"
     t.string "native_language"
     t.string "specialty"
-
     t.string "video"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "avatar"
-
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "meetings", "users", column: "users_id"
+  add_foreign_key "messages", "users"
   add_foreign_key "reviews", "users"
   add_foreign_key "slots", "meetings"
   add_foreign_key "slots", "users"
