ab6a4872c2e24a7f686d344bca76e179f6ccffe0
Armen <armen_bagramian@gmail.com>
2019-05-28 13:53:17 +0200

    Add dotenv - Protect my secret data in .env file

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
+
   get 'users/show'
   devise_for :users
   root to: 'pages#home'
-  
+
   resources :users, only: [:show] do
     resources :reviews, only: [:index, :create, :new, :update, :edit]
   end
   resources :meetings, only: [:index, :create, :new, :update, :edit, :delete]
   resources :slots, only: [:index, :create, :new, :update, :edit]
-  
+
 end
 
-  
