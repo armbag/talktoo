3440eeb40e94b0ee40ca33fd2143c37affe717f5
Armen <armen_bagramian@gmail.com>
2019-05-29 15:12:50 +0200

    linkedin authentication

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
 
   get 'messages/create'
   get 'users/show'
-  devise_for :users
+  devise_for :users, controllers: {
+    omniauthcallbacks: 'users/omniauth_callbacks'
+  }
+
   root to: 'pages#home'
 
   resources :chat_rooms, only: :show do
