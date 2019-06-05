a509f0b6e47fb54f7ae55cf67fd30e0a008f1b23
Julz <julia.gandit@gmail.com>
2019-06-05 09:15:41 +0200

    updated

--- app/javascript/plugins/init_select2.js
+++ app/javascript/plugins/init_select2.js
@@ -6,11 +6,15 @@ const initSelect2 = () => {
   $('.select2').select2(); // (~ document.querySelectorAll)
 };
 
-
+$(document).ready(function() {
+  $('search').select2({
+    placeholder: "Choose a person",
+    allowClear: true
+  });
+});
 // $("#elem").select2({theme:"select"});
 
 export { initSelect2 };
-
 
 
 
