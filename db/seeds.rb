5d67d7c9cacc1a6508ae98b3a551899e0fb43f12
Alina <alina.gotcherian@gmail.com>
2019-06-06 12:21:55 +0200

    Alex specific seeds, a bit more users

--- db/seeds.rb
+++ db/seeds.rb
@@ -66,7 +66,6 @@ user6 = User.create!({
   password: "secret",
   bio: "Award-winning pop culture buff. Subtly charming social media enthusiast. Falls down a lot.",
   native_language: "English",
-  specialty: "business",
   remote_avatar_url: "https://images.unsplash.com/photo-1553907725-c3d2e2ccc00e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80"
 })
 
@@ -85,7 +84,6 @@ user8 = User.create!({
   password: "secret",
   bio: "Award-winning pop culture buff. Subtly charming social media enthusiast. Falls down a lot.",
   native_language: "English",
-  specialty: "business",
   remote_avatar_url: "https://images.unsplash.com/photo-1507907709363-97dae7783dc9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1052&q=80"
 })
 
@@ -95,7 +93,6 @@ user9 = User.create!({
   password: "secret",
   bio: "Hi! I teach while I run my own small business. Coffee geek. Devoted explorer.",
   native_language: "English",
-  specialty: "business",
   remote_avatar_url: "https://images.unsplash.com/photo-1524772128034-3ecf9a73cbe9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80"
 })
 
@@ -105,7 +102,6 @@ user10 = User.create!({
   password: "secret",
   bio: "Award-winning pop culture buff. Subtly charming social media enthusiast.",
   native_language: "English",
-  specialty: "business",
   remote_avatar_url: "https://images.unsplash.com/photo-1508341591423-4347099e1f19?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80"
 })
 
@@ -115,7 +111,6 @@ user11 = User.create!({
   password: "secret",
   bio: "Hi! I teach while I run my own small business. Coffee geek. Avid travel evangelist. Professional gamer. Devoted explorer. Proud web aficionado.",
   native_language: "English",
-  specialty: "business",
   remote_avatar_url: "https://images.unsplash.com/photo-1512253229843-7da434b94f25?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=618&q=80"
 })
 
@@ -125,7 +120,6 @@ user12 = User.create!({
   password: "secret",
   bio: "Entrepreneur. Beer guru. Problem solver. Music enthusiast. Bacon fan.",
   native_language: "English",
-  specialty: "business",
   remote_avatar_url: "https://images.unsplash.com/photo-1522529599102-193c0d76b5b6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
 })
 
@@ -135,7 +129,6 @@ user13 = User.create!({
   password: "secret",
   bio: "Hi! I teach while I run my own small business. Coffee geek. Avid travel evangelist. Professional gamer. Devoted explorer. Proud web aficionado.",
   native_language: "English",
-  specialty: "business",
   remote_avatar_url: "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
 })
 
@@ -145,7 +138,6 @@ user14 = User.create!({
   password: "secret",
   bio: "Social media trailblazer. Pop culture ninja.",
   native_language: "English",
-  specialty: "business",
   remote_avatar_url: "https://images.unsplash.com/photo-1544168190-79c17527004f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80"
 })
 
@@ -155,7 +147,6 @@ user15 = User.create!({
   password: "secret",
   bio: "Entrepreneur. Zombie expert. Beer guru. Problem solver. Music enthusiast. Bacon fan.",
   native_language: "English",
-  specialty: "business",
   remote_avatar_url: "https://images.unsplash.com/photo-1477118476589-bff2c5c4cfbb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
 })
 
@@ -165,7 +156,6 @@ user16 = User.create!({
   password: "secret",
   bio: "Award-winning pop culture buff. Subtly charming social media enthusiast. Falls down a lot.",
   native_language: "English",
-  specialty: "business",
   remote_avatar_url: "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80"
 })
 
@@ -175,7 +165,6 @@ user17 = User.create!({
   password: "secret",
   bio: "Hi! I teach while I run my own small business. Coffee geek. Avid travel evangelist. Professional gamer. Devoted explorer. Proud web aficionado.",
   native_language: "English",
-  specialty: "business",
   remote_avatar_url: "https://images.unsplash.com/photo-1551727974-8af20a3322f1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
 })
 
@@ -185,7 +174,6 @@ user18 = User.create!({
   password: "secret",
   bio: "Award-winning pop culture buff. Subtly charming social media enthusiast. Falls down a lot.",
   native_language: "English",
-  specialty: "business",
   remote_avatar_url: "https://images.unsplash.com/photo-1555728750-9c6ad51b968d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=624&q=80"
 })
 
@@ -195,7 +183,6 @@ user19 = User.create!({
   password: "secret",
   bio: "Hi! I teach while I run my own small business. Coffee geek. Avid travel evangelist. Professional gamer. Devoted explorer. Proud web aficionado.",
   native_language: "English",
-  specialty: "business",
   remote_avatar_url: "https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80"
 })
 
@@ -205,7 +192,6 @@ user20 = User.create!({
   password: "secret",
   bio: "Entrepreneur. Zombie expert. Beer guru. Problem solver. Music enthusiast. Bacon fan.",
   native_language: "English",
-  specialty: "business",
   remote_avatar_url: "https://images.unsplash.com/photo-1520409364224-63400afe26e5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=658&q=80"
 })
 
@@ -218,11 +204,75 @@ user21 = User.create!({
   avatar: File.open("#{Rails.root}/app/assets/images/armen_photo.jpg")
 })
 
+
+user22 = User.create!({
+  name: Faker::Name.female_first_name,
+  email: Faker::Internet.free_email,
+  password: "secret",
+  bio: "Hey! I teach English while I run my own small business. Coffee geek. Avid travel evangelist. Professional gamer. Devoted explorer. Proud web aficionado.",
+  native_language: "English",
+  remote_avatar_url: "https://images.unsplash.com/photo-1463763422025-e07f8081b9c9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=633&q=80"
+})
+
+user23 = User.create!({
+  name: Faker::Name.male_first_name,
+  email: Faker::Internet.free_email,
+  password: "secret",
+  bio: "Entrepreneur. Zombie expert. Beer guru. Problem solver. Music enthusiast. Bacon fan.",
+  native_language: "English",
+  remote_avatar_url: "https://images.unsplash.com/photo-1536548665027-b96d34a005ae?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
+})
+
+user24 = User.create!({
+  name: Faker::Name.female_first_name,
+  email: Faker::Internet.free_email,
+  password: "secret",
+  bio: "Hi! I teach while I run my own small business. Coffee geek. Avid travel evangelist. Professional gamer. Devoted explorer. Proud web aficionado.",
+  native_language: "English",
+  remote_avatar_url: "https://images.unsplash.com/photo-1489424731084-a5d8b219a5bb?ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80"
+})
+
+user25 = User.create!({
+  name: Faker::Name.male_first_name,
+  email: Faker::Internet.free_email,
+  password: "secret",
+  bio: "Entrepreneur. Zombie expert. Beer guru. Problem solver. Music enthusiast. Bacon fan.",
+  native_language: "English",
+  remote_avatar_url: "https://images.unsplash.com/photo-1476657680631-c07285ff2581?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1010&q=80"
+})
+
+user26= User.create!({
+  name: Faker::Name.male_first_name,
+  email: Faker::Internet.free_email,
+  password: "secret",
+  bio: "Entrepreneur. Zombie expert. Beer guru. Problem solver. Music enthusiast. Bacon fan.",
+  native_language: "English",
+  remote_avatar_url: "https://images.unsplash.com/photo-1509112552557-8eb3dab85cfc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1049&q=80"
+})
+
+user27 = User.create!({
+  name: Faker::Name.female_first_name,
+  email: Faker::Internet.free_email,
+  password: "secret",
+  bio: "Hi! I'm a small-business owner and I love to teach. Coffee geek. Avid travel evangelist. Devoted explorer. Proud web aficionado.",
+  native_language: "English",
+  remote_avatar_url: "https://images.unsplash.com/photo-1508214751196-bcfd4ca60f91?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"
+})
+
+user28 = User.create!({
+  name: Faker::Name.male_first_name,
+  email: Faker::Internet.free_email,
+  password: "secret",
+  bio: "Entrepreneur and University of California business professor. Problem solver. Music enthusiast.",
+  native_language: "English",
+  remote_avatar_url: "https://images.unsplash.com/photo-1553267751-1c148a7280a1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80"
+})
+
 user4 = User.create!({
   name: "Alex",
   email: "alexc@gmail.com",
   password: "secret",
-  bio: "Award-winning pop culture buff. Subtly charming social media enthusiast. Loves running.",
+  bio: "PHP guru and full-stack developer. Loves running. I teach while running my own business.",
   native_language: "English",
   avatar: File.open("#{Rails.root}/app/assets/images/Alex.png")
 })
--- lib/tasks/create_slots.rake
+++ lib/tasks/create_slots.rake
@@ -6,14 +6,105 @@ namespace :db do
 
     start_time = DateTime.parse("June 3, 12:00 AM")
 
-    until start_time > DateTime.parse("June 9, 11:45 PM")
+    # All users except Alex =
+    alex = User.find_by email: "alexc@gmail.com"
+    users = User.where.not(email: "alexc@gmail.com")
+
+    until start_time > DateTime.parse("June 16, 11:45 PM")
       Slot.create!(
         start: start_time,
         end: start_time + 15.minutes,
-        teacher_id: User.all.ids.sample,
+        teacher_id: users.map { |user| user.id }.sample,
         price: (rand(5) + 10)
       )
       start_time += 15.minutes
     end
+
+    #Create slots for Alex after June 8
+    start_time = DateTime.parse("June 8, 12:00 AM")
+
+    until start_time > DateTime.parse("June 16, 11:45 PM")
+      Slot.create!(
+        start: start_time,
+        end: start_time + 15.minutes,
+        teacher_id: alex.id,
+        price: 12
+      )
+      start_time += 180.minutes
+    end
+
+    #Create slots for Alex on June 7
+    Slot.create!({
+      start: DateTime.parse("June 7, 5:15 pm"),
+      end: start_time + 15.minutes,
+      teacher_id: alex.id,
+      price: 12
+    })
+    Slot.create!({
+        start: DateTime.parse("June 7, 5:30 pm"),
+        end: start_time + 15.minutes,
+        teacher_id: alex.id,
+        price: 12
+    })
+    Slot.create!({
+      start: DateTime.parse("June 7, 5:45 pm"),
+      end: start_time + 15.minutes,
+      teacher_id: alex.id,
+      price: 12
+    })
+    Slot.create!({
+        start: DateTime.parse("June 7, 6:00 pm"),
+        end: start_time + 15.minutes,
+        teacher_id: alex.id,
+        price: 12
+    })
+    Slot.create!({
+        start: DateTime.parse("June 7, 6:15 pm"),
+        end: start_time + 15.minutes,
+        teacher_id: alex.id,
+        price: 12
+    })
+    Slot.create!({
+      start: DateTime.parse("June 7, 6:30 pm"),
+      end: start_time + 15.minutes,
+      teacher_id: alex.id,
+      price: 12
+    })
+    Slot.create!({
+        start: DateTime.parse("June 7, 6:45 pm"),
+        end: start_time + 15.minutes,
+        teacher_id: alex.id,
+        price: 12
+    })
+    Slot.create!({
+        start: DateTime.parse("June 7, 7:00 pm"),
+        end: start_time + 15.minutes,
+        teacher_id: alex.id,
+        price: 12
+    })
+    Slot.create!({
+        start: DateTime.parse("June 7, 7:15 pm"),
+        end: start_time + 15.minutes,
+        teacher_id: alex.id,
+        price: 12
+    })
+    Slot.create!({
+      start: DateTime.parse("June 7, 7:30 pm"),
+      end: start_time + 15.minutes,
+      teacher_id: alex.id,
+      price: 12
+    })
+    Slot.create!({
+        start: DateTime.parse("June 7, 7:45 pm"),
+        end: start_time + 15.minutes,
+        teacher_id: alex.id,
+        price: 12
+    })
+    Slot.create!({
+        start: DateTime.parse("June 7, 8:00 pm"),
+        end: start_time + 15.minutes,
+        teacher_id: alex.id,
+        price: 12
+    })
   end
 end
