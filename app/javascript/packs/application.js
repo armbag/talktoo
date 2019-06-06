83b7758ac7b9521dcdf316f347329207d2f99770
Armen <armen_bagramian@gmail.com>
2019-06-06 11:29:00 +0200

    start rating to be finished

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,5 +1,9 @@
 import "bootstrap";
 import { initSelect2 } from '../plugins/init_select2.js';
+import { initStarRating } from '../plugins/init_star_rating';
 
 
 initSelect2()
+
+
+initStarRating();
